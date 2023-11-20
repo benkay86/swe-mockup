5b9d5308e0e3266911e2354f26a2771adbe609d9
Benjamin Kay <benjamin@benkay.net>
2023-11-20 13:30:00 -0600

    Synchronize access to cov_b to reduce memory use

--- src/bin/benchmark.rs
+++ src/bin/benchmark.rs
@@ -4,12 +4,84 @@
 extern crate blas_src;
 extern crate lapack_src;
 
-use ndarray::{s, Array, Axis, NewAxis};
+use ndarray::{s, Array, Axis, Dimension, NewAxis, ShapeBuilder};
 use ndarray_rand::{RandomExt, SamplingStrategy};
+use num_traits::Zero;
 use rand_distr::{Distribution, StandardNormal, Uniform};
 use rayon::iter::{IndexedParallelIterator, IntoParallelIterator, ParallelIterator};
 use rayon::ThreadPoolBuilder;
 use std::io::Write; // for flushing stdout
+use std::sync::{Condvar, Mutex};
+
+// Structures for thread synchronization.
+
+// Variables to be guarded by the mutex.
+#[derive(Debug)]
+struct CovBMutexInner<S, D>
+where
+    D: Dimension,
+{
+    // Number of inner pools about to be run. Used to ensure there are not more
+    // inner pools running than the number of cpus in the outer pool.
+    inner_pools_reserved: usize,
+    // Number of inner pools running (or about to start). Used to eliminate lock
+    // contention on the inner therad pool.
+    inner_pools_running: usize,
+    // The variance-covariance matrix of b.
+    cov_b: Array<S, D>,
+}
+impl<S, D> CovBMutexInner<S, D>
+where
+    S: Clone + Zero,
+    D: Dimension,
+{
+    // Initialize `cov_b` to a matrix of zeros with shape `shape`.
+    fn zeros<Sh>(shape: Sh) -> Self
+    where
+        Sh: ShapeBuilder<Dim = D>,
+    {
+        Self {
+            // Initially there are no inner pools reserved.
+            inner_pools_reserved: 0,
+            // Initially there are no inner pools running.
+            inner_pools_running: 0,
+            // Start with a matrix of zeros and add each block of the SwE.
+            cov_b: Array::<S, D>::zeros(shape),
+        }
+    }
+}
+
+// Variables associated with cov_b and its condition variable.
+// Needed for working around https://github.com/rayon-rs/rayon/issues/1105
+#[derive(Debug)]
+struct CovBCondvar<S, D>
+where
+    D: Dimension,
+{
+    // Mutex
+    mutex: Mutex<CovBMutexInner<S, D>>,
+    // Condition variable for `inner_pools_reserved`
+    condvar_reserved: Condvar,
+    // Condition variable for `inner_pools_running`
+    condvar_running: Condvar,
+}
+impl <S, D> CovBCondvar<S, D>
+where
+    S: Clone + Zero,
+    D: Dimension,
+{
+    // Initialize `mutex.cov_b` to a matrix of zeros with shape `shape`.
+    fn zeros<Sh>(shape: Sh) -> Self
+    where
+        Sh: ShapeBuilder<Dim = D>,
+    {
+        Self {
+            mutex: Mutex::new(CovBMutexInner::zeros(shape)),
+            condvar_reserved: Condvar::new(),
+            condvar_running: Condvar::new(),
+        }
+    }
+}
 
 #[allow(non_upper_case_globals)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -100,6 +172,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Decide how many cpu cores to assign to the inner and outer thread pools.
     // The more cpus we use on the outer loop the more memory we will need.
+    // Because each inner thread pool blocks on a mutex, there is no performance
+    // improvement from using more than two threads for the outer pool.
     let ncpus = num_cpus::get();
     let ncpus_outer = if ncpus < 5 { 1 } else { 2 };
     let ncpus_inner = std::cmp::max(1, ncpus - ncpus_outer);
@@ -117,6 +191,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Outer thread pool has {} cpus.", ncpus_outer);
     println!("Inner thread pool has {} cpus.", ncpus_inner);
 
+    // Initialize cov_b to a matrix of zeros and set up some synchronization
+    // primitives around it.
+    let cov_b_condvar = CovBCondvar::<f64, _>::zeros((n_pred, n_pred, n_feat));
+
     // Compute the variance-covariance matrix of the regression coefficients, //
     // B, using the sandwhich estimator. //
     print!("Computing SwE... ");
@@ -126,18 +204,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let time = std::time::Instant::now();
 
     // Repeatedly compute cov_b n_rep times.
-    let mut cov_b = Array::<f64, _>::zeros((n_pred, n_pred, n_feat));
     for _ in 0..n_rep {
-        // Assign cov_b the result from the outer thread pool.
+        // Reset cov_b to a matrix of zeros for this repetition.
+        {
+            cov_b_condvar.mutex.lock().unwrap().cov_b.fill(0.);
+            // Lock is dropped and released here at end of scope.
+        }
+
         // Enter the outer thread pool.
-        cov_b = pool_outer.install(|| {
+        pool_outer.install(|| {
             // Iterate over blocks.
             std::ops::Range {
                 start: 0,
                 end: n_blocks + 1, // Rust ranges exclude the last element
             }
             .into_par_iter()
-            .map(|block_id| {
+            .for_each(|block_id| {
+                // Workaround for https://github.com/rayon-rs/rayon/issues/1105
+                // Only panics if mutex is poisoned.
+                {
+                    // Wait until cpu resources are available.
+                    let mut lock = cov_b_condvar.mutex.lock().unwrap();
+                    while lock.inner_pools_running >= ncpus_outer {
+                        lock = cov_b_condvar.condvar_reserved.wait(lock).unwrap();
+                    }
+                    // Increment the number of inner pools reserved.
+                    lock.inner_pools_reserved += 1;
+                    // Lock is dropped and released here at end of scope.
+                }
+
                 // Find indices for observations in this block.
                 // This is an opportunity for optimization, see https://github.com/rust-ndarray/ndarray/issues/466
                 // However, this would require major changes to ndarray :-(
@@ -151,16 +246,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     .select(Axis(1), &block_indices)
                     .dot(&resid.select(Axis(0), &block_indices));
 
+                // Don't enter the inner thread pool until the mutex is
+                // available.
+                {
+                    // Wait until no other inner pools are running.
+                    let mut lock = cov_b_condvar.mutex.lock().unwrap();
+                    while lock.inner_pools_running > 0 {
+                        lock = cov_b_condvar.condvar_running.wait(lock).unwrap();
+                    }
+                    // Increment the number of inner pools running.
+                    lock.inner_pools_running += 1;
+                    // Lock is dropped and released here at end of scope.
+                }
+
                 // Enter the inner thread pool.
                 pool_inner.install(|| {
-                    // Allocate memory for cov_b for this block.
-                    let mut cov_b = Array::<f64, _>::zeros((n_pred, n_pred, n_feat));
+                    // Lock the mutex to get exclusive access to cov_b.
+                    // Only panics if the mutex is poisoned.
+                    let cov_b_mutex_inner = &mut (*cov_b_condvar.mutex.lock().unwrap());
+                    let cov_b = &mut cov_b_mutex_inner.cov_b;
 
                     // Iterate over the features in cov_b and half_sandwich
                     // together. Zipping together the axis iterators proves to
                     // the compiler that we will not go out of bounds,
                     // eliminating the need for runtime bounds checking.
-                    cov_b
+                     cov_b
                         // Iterate over axis 2 (3rd dimension) of cov_b...
                         .axis_iter_mut(Axis(2))
                         .into_par_iter()
@@ -177,15 +287,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                             cov_b += &half_sandwich.dot(&half_sandwich.t());
                         });
 
-                    // Return cov_b for this block.
-                    cov_b
-                })
+                    // Workaround for https://github.com/rayon-rs/rayon/issues/1105
+                    // Decrement the number of inner pools running.
+                    cov_b_mutex_inner.inner_pools_running -= 1;
+                    // Signal to a blocking thread that this inner pool is done.
+                    cov_b_condvar.condvar_running.notify_one();
+                    // Decrement the number of inner pools reserved.
+                    cov_b_mutex_inner.inner_pools_reserved -= 1;
+                    // Signal to a blocking thread that this inner pool is done.
+                    cov_b_condvar.condvar_reserved.notify_one();
+                    // Lock is dropped and released here at end of scope.
+                });
             })
-            // Sum cov_b over blocks.
-            .reduce_with(|a, b| a + b)
-            // Return cov_b filled with zeros if the iterator was empty.
-            .unwrap_or_else(|| Array::<f64, _>::zeros((n_pred, n_pred, n_feat)))
-        });
+        })
     }
 
     // Print the time elapsed.
@@ -195,6 +309,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         "That's {:?} per repetition.",
         time_elapsed / n_rep.try_into().unwrap()
     );
+
+    // We're done multithreading; take cov_b out of the mutex.
+    let cov_b = cov_b_condvar.mutex.into_inner().unwrap().cov_b; // panic if mutex is poisoned
 
     // Print an element of cov_b to make sure the optimizer sees we're using its
     // value and doesn't optimize away our benchmark!
